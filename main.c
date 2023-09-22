e756fceb722882486f14d17fa4f8918bb77c2aeb
Leviathan <86807700+Leviathenn@users.noreply.github.com>
2023-09-22 20:09:15 +0000

    trippin

--- main.c
+++ main.c
@@ -26,15 +26,27 @@ void string2hexString(char* input, char* output)
 
 int main(int argc, char const *argv[]){
     
+    if(argc < 4 || argc == 1 ){
+        puts("./odo <file> <out> <options>");
+    }else{
+
+    }
+
+    char* inputFile = argv[1];
+    puts(inputFile);
+
+    
+    
+
 
     float SectionHeader[] = {0x2e,0x53,0x45,0x43,0x54,0x49,0x4f,0x4e,0x20,0x4f,0x44,0x4f,0x48,0x45,0x41,0x44,0x45,0x52};
 
     char* inputD = "Hello, World!";
     char* outputD;
 
-    string2hexString(inputD, outputD);
+    //string2hexString(inputD, outputD);
 
-    puts(outputD);    
+    //puts(outputD);    
     
 
     
